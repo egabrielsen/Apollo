2dd514df4762614a71ac404034efd0a3048ed203
egabrielsen <erik.gabe@gmail.com>
2015-10-30 02:12:25 -0500

    setting up repo

---
+++ app/main/config/dependencies.rb
@@ -0,0 +1,8 @@
+# Specify which components you wish to include when
+# the "home" component loads.
+
+# bootstrap css framework
+
+
+# provides templates for login, signup, and logout
+component 'user_templates'
